f615aab41807e9201c227a8776c2fd24d8cdeeee
EJ Quance <69334632+CloudMonkeyCoding@users.noreply.github.com>
2025-09-23 12:43:00 +0800

    Fix category image paths

--- userSide/PRODUCT/bread.php
+++ userSide/PRODUCT/bread.php
@@ -20,10 +20,9 @@ if ($pdo) {
 
     <div class="products-grid">
       <?php foreach ($products as $product): ?>
+        <?php $imageUrl = getProductImageUrl($product, '../../'); ?>
         <div class="product-card" onclick="goToProduct(<?= htmlspecialchars($product['Product_ID']) ?>)">
-          <?php if (!empty($product['Image_Path'])): ?>
-            <img src="../../adminSide/products/uploads/<?= htmlspecialchars($product['Image_Path']) ?>" alt="<?= htmlspecialchars($product['Name']) ?>">
-          <?php endif; ?>
+          <img src="<?= htmlspecialchars($imageUrl) ?>" alt="<?= htmlspecialchars($product['Name']) ?>">
           <div class="product-name"><?= htmlspecialchars($product['Name']) ?></div>
           <div class="price-stock">Price: ₱<?= htmlspecialchars(number_format((float)$product['Price'], 2)) ?><br>Stock: <?= htmlspecialchars($product['Stock_Quantity']) ?></div>
           <div class="buttons">
